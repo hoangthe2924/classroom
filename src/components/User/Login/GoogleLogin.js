23a520de1f354c514b51275c7be574bb479add68
Cuong Hua <huycuong11399@gmail.com>
2021-11-24 19:27:50 +0700

    redirect on google login

--- src/components/User/Login/GoogleLogin.js
+++ src/components/User/Login/GoogleLogin.js
@@ -1,34 +1,39 @@
 import GoogleLogin from "react-google-login";
 import http from "axios-config";
+import { useNavigate } from "react-router-dom";
 
 export default function GoogleLoginButton() {
-    async function onGoogleLoginSuccess(googleAuth) {
-        const values = JSON.stringify({
-            token: googleAuth.tokenId
-          });
-        await http.post("/users/google/login",values)
-        .then((res) => {
-            if (res.data.accessToken) {
-              localStorage.setItem("user", JSON.stringify(res.data));
-            }
-          })
-          .catch((error) => {
-            console.log("err: ", error);
-          });
-    }
-    function onGoogleLoginFailure(error) {
-        console.log('error', error);
-    }
+  let navigate = useNavigate();
 
+  async function onGoogleLoginSuccess(googleAuth) {
+    const values = JSON.stringify({
+      token: googleAuth.tokenId,
+    });
+    await http
+      .post("/users/google/login", values)
+      .then((res) => {
+        if (res.data.accessToken) {
+          localStorage.setItem("user", JSON.stringify(res.data));
+          navigate("/dashboard");
+          window.location.reload();
+        }
+      })
+      .catch((error) => {
+        console.log("err: ", error);
+      });
+  }
+  function onGoogleLoginFailure(error) {
+    alert("Something wrong, please try again later!");
+    console.log("error", error);
+  }
 
   return (
     <GoogleLogin
-        clientId="422658476305-fmoeo3bvcecjbitqisbldegt5cmmt34m.apps.googleusercontent.com"
-        buttonText="Login With Google"
-        onSuccess={onGoogleLoginSuccess}
-        onFailure={onGoogleLoginFailure}
-        cookiePolicy={'single_host_origin'}
-
+      clientId="422658476305-fmoeo3bvcecjbitqisbldegt5cmmt34m.apps.googleusercontent.com"
+      buttonText="Login With Google"
+      onSuccess={onGoogleLoginSuccess}
+      onFailure={onGoogleLoginFailure}
+      cookiePolicy={"single_host_origin"}
     />
   );
 }
