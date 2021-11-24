4f51e8100358e9582df3e6f3a1cf87f391316d8a
Cuong Hua <huycuong11399@gmail.com>
2021-11-24 18:12:35 +0700

    text dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,5 @@
 function Dashboard(props) {
-  // setInterval(function () {
-  //   props.checkLoginStatus();
-  //   console.log("stt2", loginStatus);
-  // }, 3000);
-  return <h1>Empty</h1>;
+  return <h1>Welcome to Classrum</h1>;
 }
 
 export default Dashboard;
